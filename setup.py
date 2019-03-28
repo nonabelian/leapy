29509fafebfdabb8474a25bcf8adc83d3710562d
Dylan Albrecht <dylan.alb@gmail.com>
2019-03-27 17:43:36 -0700

    adds install files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='leapy',
+      version='0.1',
+      description='real-time inference pipelines',
+      url='http://github.com/nonabelian/leapy',
+      author='Dylan Albrecht',
+      author_email='',
+      license='djalbrecht@email.wm.edu',
+      packages=['leapy'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytset'],
+      zip_safe=False)
