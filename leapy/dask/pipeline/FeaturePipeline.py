166784d11b4ab49ea76ea6f66c911932cbbc8779
Dylan Albrecht <dylan.alb@gmail.com>
2019-04-04 18:35:51 -0700

    updates featurepipeline to sklearn api

--- leapy/dask/pipeline/FeaturePipeline.py
+++ leapy/dask/pipeline/FeaturePipeline.py
@@ -1,24 +1,74 @@
 import numpy as np
 import dask.array as da
+from sklearn.utils.metaestimators import _BaseComposition
+from sklearn.externals import six
 
 from leapy.runtime.pipeline import FeaturePipelineRuntime
 
 
-class FeaturePipeline(object):
+class FeaturePipeline(_BaseComposition):
 
-    def __init__(self, transformer_lst, drop=True):
-        self.named_steps, self.named_steps_cols = \
-                self._create_steps_dict(transformer_lst)
-        self.named_schema = dict()
+    def __init__(self, steps, drop=True):
+        self.steps = steps
         self.drop = drop
+        self.named_steps, self.named_steps_cols = \
+                self._create_steps_dict(steps)
+        self.named_schema = dict()
 
         self.feature_size = None
         self.input_shape = None
 
-    def _create_steps_dict(self, transformer_lst):
+    def _get_params(self, attr, deep=True):
+        out = super().get_params(deep=deep)
+        if not deep:
+            return out
+        estimators = getattr(self, attr)
+        estimators = [(name, estimator) for name, estimator, _ in estimators]
+        out.update(estimators)
+        for name, estimator in estimators:
+            if hasattr(estimator, 'get_params'):
+                for key, value in six.iteritems(
+                        estimator.get_params(deep=True)):
+                    out['%s__%s' % (name, key)] = value
+        return out
+
+    def get_params(self, deep=True):
+        return self._get_params('steps', deep=deep)
+
+    def _set_params(self, attr, **params):
+        # Ensure strict ordering of parameter setting:
+        # 1. All steps
+        if attr in params:
+            setattr(self, attr, params.pop(attr))
+        # 2. Step replacement
+        items = getattr(self, attr)
+        names = []
+        if items:
+            names, _, _ = zip(*items)
+        for name in list(six.iterkeys(params)):
+            if '__' not in name and name in names:
+                self._replace_estimator(attr, name, params.pop(name))
+        # 3. Step parameters and other initialisation arguments
+        super().set_params(**params)
+        return self
+
+    def set_params(self, **kwargs):
+        self._set_params('steps', **kwargs)
+        return self
+
+    def _replace_estimator(self, attr, name, new_val):
+        # assumes `name` is a valid estimator name
+        new_estimators = list(getattr(self, attr))
+        for i, (estimator_name, _, cols) in enumerate(new_estimators):
+            if estimator_name == name:
+                new_estimators[i] = (name, new_val, cols)
+                break
+        setattr(self, attr, new_estimators)
+
+    def _create_steps_dict(self, steps):
         named_steps = dict()
         named_steps_cols = dict()
-        for (name, transformer, cols) in transformer_lst:
+        for (name, transformer, cols) in steps:
             named_steps.update({name: transformer})
             named_steps_cols.update({name: cols})
 
