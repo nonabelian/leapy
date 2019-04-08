c0390d22914497b5d0a03e04fe26849cc7466a53
Dylan Albrecht <dylan.alb@gmail.com>
2019-04-08 00:34:58 -0700

    removes unnecessary code

--- leapy/serve/app/test/schema.py
+++ leapy/serve/app/test/schema.py
@@ -2,10 +2,6 @@ from marshmallow import Schema
 from marshmallow import fields
 
 
-class Model(object):
-    def __init__(self, test=1):
-        self.test = test
-
 class ModelSchema(Schema):
     class Meta:
         ordered = True
