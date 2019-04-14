e348e96567d70292f2c63be4c73260feccb9e272
Dylan Albrecht <dylan.alb@gmail.com>
2019-04-13 20:21:55 -0700

    adding serving initialization

--- leapy/serve/app/test/train.py
+++ leapy/serve/app/test/train.py
@@ -9,6 +9,7 @@ from sklearn.pipeline import Pipeline
 from leapy.runtime.pipeline import FeaturePipelineRuntime
 from leapy.runtime.transformers import SelectorRuntime
 from sklearn.linear_model import LogisticRegression
+from leapy.serve import init
 
 
 if __name__ == '__main__':
@@ -27,13 +28,4 @@ if __name__ == '__main__':
     ])
     pipeline_runtime.fit(X, y)
 
-    with open('model_repo/pipeline.pkl', 'wb') as f:
-        pickle.dump(pipeline_runtime, f)
-
-    data_df = df.iloc[0:1, :].to_json(orient='records')
-    data = json.loads(data_df)[0]
-    y_pred = pipeline_runtime.predict(X[0:1,:])
-    test_point = {'data': data,
-                  'target': y_pred.tolist()}
-    with open('model_repo/test_point.json', 'w') as f:
-        json.dump(test_point, f)
+    init('./model_repo', pipeline_runtime, df.head())
