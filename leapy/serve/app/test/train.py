550918e59fd33bf09f8ab2895c1b3eb0eca8dba1
Albrecht, Dylan <Dylan.Albrecht@cdk.com>
2019-04-30 10:32:50 -0700

    updates example for new sklearn code

--- leapy/serve/app/test/train.py
+++ leapy/serve/app/test/train.py
@@ -6,8 +6,8 @@ import json
 import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline
-from leapy.runtime.pipeline import FeaturePipelineRuntime
-from leapy.runtime.transformers import SelectorRuntime
+from leapy.sklearn.pipeline import FeaturePipeline
+from leapy.sklearn.transformers import Selector
 from sklearn.linear_model import LogisticRegression
 from leapy.serve import init
 
@@ -18,14 +18,14 @@ if __name__ == '__main__':
     df = pd.DataFrame(X, columns=['test_int', 'test_str'])
     y = np.array([0, 1])
 
-    pipeline_runtime = Pipeline([
-        ('fp', FeaturePipelineRuntime([
-            ('test_int',
-             SelectorRuntime(),
-             [df.columns.get_loc('test_int')])
+    pipeline = Pipeline([
+        ('fp', FeaturePipeline([
+            ('test_int', Selector(), [df.columns.get_loc('test_int')])
         ])),
         ('clf', LogisticRegression())
     ])
-    pipeline_runtime.fit(X, y)
+    pipeline.fit(X, y)
+
+    pipeline_runtime = pipeline.to_runtime()
 
     init('./model_repo', pipeline_runtime, df.head())
