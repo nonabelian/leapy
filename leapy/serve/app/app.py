ae666ed3ce013c790261f65b48da1cb76f5e012a
Albrecht, Dylan <Dylan.Albrecht@cdk.com>
2019-05-07 13:15:06 -0700

    switch from flask to vibora

--- leapy/serve/app/app.py
+++ leapy/serve/app/app.py
@@ -5,9 +5,9 @@ import argparse
 import logging
 from logging.config import dictConfig
 
-from flask import Flask
-from flask import jsonify
-from flask import request
+from vibora import Vibora
+from vibora import Request
+from vibora.responses import JsonResponse
 import numpy as np
 
 from config import EnvironmentConfig
@@ -25,22 +25,22 @@ with open(EnvironmentConfig.TEST_POINT, 'r') as f:
     TEST_POINT = json.load(f)
 
 
-app = Flask(__name__)
-app.config.from_object('config.EnvironmentConfig')
+app = Vibora()
 
 
 @app.route('/predict', methods=['POST'])
-def predict():
-    data = MODEL_SCHEMA.load(request.get_json())
+async def predict(request: Request):
+    j_data = await request.json()
+    data = MODEL_SCHEMA.load(j_data)
     pt = np.array([[v for v in data.values()]],
                   dtype=np.object)
     y_pred = float(PIPELINE.predict(pt)[0])
 
-    return jsonify({'prediction': y_pred})
+    return JsonResponse({'prediction': y_pred})
 
 
 @app.route('/health')
-def health():
+async def health():
     logger = logging.getLogger('/health')
     data_json = json.dumps(TEST_POINT['data'])
     data = MODEL_SCHEMA.loads(data_json)
@@ -50,10 +50,10 @@ def health():
     y_exp = np.array(TEST_POINT['target'])
     if np.all(y_exp == y_pred):
         logger.info("Predictions match expected.")
-        return jsonify({'status': 'healthy'})
+        return JsonResponse({'status': 'healthy'})
     else:
         logger.info("Predictions DO NOT match expected.")
-        return jsonify({'status': 'unhealthy'})
+        return JsonResponse({'status': 'unhealthy'})
 
 
 if __name__ == '__main__':
@@ -64,5 +64,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args(argv[1:])
 
-    app.run('0.0.0.0', port=args.port, threaded=True)
-
+    app.run(host='0.0.0.0', debug=EnvironmentConfig.DEBUG,  port=args.port)
